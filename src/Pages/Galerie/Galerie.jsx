15e2441623ee9d5a9aeaa6052d498723b9795370
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-11-19 20:23:29 +0100

    debuggen

--- src/Pages/Galerie/Galerie.jsx
+++ src/Pages/Galerie/Galerie.jsx
@@ -3,24 +3,24 @@ import "../../Pages/Galerie/Galerie.css";
 
 // Mini-Hasen
 
-import MiniHasen1 from "../../assets/image/miniHasen/mini1.png";
-import MiniHasen2 from "../../assets/image/miniHasen/mini2.png";
-import MiniHasen3 from "../../assets/image/miniHasen/mini3.png";
-import MiniHasen4 from "../../assets/image/miniHasen/mini4.png";
-import MiniHasen5 from "../../assets/image/miniHasen/mini5.png";
-import MiniHasen6 from "../../assets/image/miniHasen/mini6.png";
-import MiniHasen7 from "../../assets/image/miniHasen/mini7.png";
-import MiniHasen8 from "../../assets/image/miniHasen/mini8.png";
-import MiniHasen9 from "../../assets/image/miniHasen/mini9.png";
-import MiniHasen10 from "../../assets/image/miniHasen/mini10.png";
-import MiniHasen11 from "../../assets/image/miniHasen/mini11.png";
-import MiniHasen12 from "../../assets/image/miniHasen/mini12.png";
-import MiniHasen13 from "../../assets/image/miniHasen/mini13.png";
+import MiniHasen1 from "../../assets/image/mini1.png";
+import MiniHasen2 from "../../assets/image/mini2.png";
+import MiniHasen3 from "../../assets/image/mini3.png";
+import MiniHasen4 from "../../assets/image/mini4.png";
+import MiniHasen5 from "../../assets/image/mini5.png";
+import MiniHasen6 from "../../assets/image/mini6.png";
+import MiniHasen7 from "../../assets/image/mini7.png";
+import MiniHasen8 from "../../assets/image/mini8.png";
+import MiniHasen9 from "../../assets/image/mini9.png";
+import MiniHasen10 from "../../assets/image/mini10.png";
+import MiniHasen11 from "../../assets/image/mini11.png";
+import MiniHasen12 from "../../assets/image/mini12.png";
+import MiniHasen13 from "../../assets/image/mini13.png";
 
 // Super-Hasen
-import SuperHasen1 from "../../assets/image/superHasen/super1.png";
-import SuperHasen2 from "../../assets/image/superHasen/super2.png";
-import SuperHasen3 from "../../assets/image/superHasen/super3.png";
+import SuperHasen1 from "../../assets/image/super1.png";
+import SuperHasen2 from "../../assets/image/super2.png";
+import SuperHasen3 from "../../assets/image/super3.png";
 
 // import SuperHasen4 from "../Galerie/Image/SuperHasen/super4.png";
 // import SuperHasen5 from "../Galerie/Image/SuperHasen/super5.png";
