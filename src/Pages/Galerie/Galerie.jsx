b62955f97d2ecc2c8af851fbded51fcd0d244dea
Tairou Mouhamed <tairou97-mouhamed@hotmail.com>
2024-11-19 19:25:01 +0100

    Gallery

--- src/Pages/Galerie/Galerie.jsx
+++ src/Pages/Galerie/Galerie.jsx
@@ -3,18 +3,18 @@ import "../../Pages/Galerie/Galerie.css";
 
 // Mini-Hasen
 import MiniHasen1 from "./Image/miniHasen/mini1.png";
-import MiniHasen2 from "../Galerie/Image/MiniHasen/mini2.png";
-import MiniHasen3 from "../Galerie/Image/MiniHasen/mini3.png";
-import MiniHasen4 from "../Galerie/Image/MiniHasen/mini4.png";
-import MiniHasen5 from "../Galerie/Image/MiniHasen/mini5.png";
-import MiniHasen6 from "../Galerie/Image/MiniHasen/mini6.png";
-import MiniHasen7 from "../Galerie/Image/MiniHasen/mini7.png";
-import MiniHasen8 from "../Galerie/Image/MiniHasen/mini8.png";
-import MiniHasen9 from "../Galerie/Image/MiniHasen/mini9.png";
-import MiniHasen10 from "../Galerie/Image/MiniHasen/mini10.png";
-import MiniHasen11 from "../Galerie/Image/MiniHasen/mini11.png";
-import MiniHasen12 from "../Galerie/Image/MiniHasen/mini12.png";
-import MiniHasen13 from "../Galerie/Image/MiniHasen/mini13.png";
+import MiniHasen2 from "./Image/miniHasen/mini2.png";
+// import MiniHasen3 from "../Galerie/Image/MiniHasen/mini3.png";
+// import MiniHasen4 from "../Galerie/Image/MiniHasen/mini4.png";
+// import MiniHasen5 from "../Galerie/Image/MiniHasen/mini5.png";
+// import MiniHasen6 from "../Galerie/Image/MiniHasen/mini6.png";
+// import MiniHasen7 from "../Galerie/Image/MiniHasen/mini7.png";
+// import MiniHasen8 from "../Galerie/Image/MiniHasen/mini8.png";
+// import MiniHasen9 from "../Galerie/Image/MiniHasen/mini9.png";
+// import MiniHasen10 from "../Galerie/Image/MiniHasen/mini10.png";
+// import MiniHasen11 from "../Galerie/Image/MiniHasen/mini11.png";
+// import MiniHasen12 from "../Galerie/Image/MiniHasen/mini12.png";
+// import MiniHasen13 from "../Galerie/Image/MiniHasen/mini13.png";
 
 // Super-Hasen
 import SuperHasen1 from "../Galerie/Image/SuperHasen/super1.png";
@@ -47,17 +47,17 @@ const Galerie = () => {
     "Mini-Hasen": [
       { src: MiniHasen1, text: "Mini-Hasen Bild 1" },
       { src: MiniHasen2, text: "Mini-Hasen Bild 2" },
-      { src: MiniHasen3, text: "Mini-Hasen Bild 3" },
-      { src: MiniHasen4, text: "Mini-Hasen Bild 4" },
-      { src: MiniHasen5, text: "Mini-Hasen Bild 5" },
-      { src: MiniHasen6, text: "Mini-Hasen Bild 6" },
-      { src: MiniHasen7, text: "Mini-Hasen Bild 7" },
-      { src: MiniHasen8, text: "Mini-Hasen Bild 8" },
-      { src: MiniHasen9, text: "Mini-Hasen Bild 9" },
-      { src: MiniHasen10, text: "Mini-Hasen Bild 10" },
-      { src: MiniHasen11, text: "Mini-Hasen Bild 11" },
-      { src: MiniHasen12, text: "Mini-Hasen Bild 12" },
-      { src: MiniHasen13, text: "Mini-Hasen Bild 13" },
+      // { src: MiniHasen3, text: "Mini-Hasen Bild 3" },
+      // { src: MiniHasen4, text: "Mini-Hasen Bild 4" },
+      // { src: MiniHasen5, text: "Mini-Hasen Bild 5" },
+      // { src: MiniHasen6, text: "Mini-Hasen Bild 6" },
+      // { src: MiniHasen7, text: "Mini-Hasen Bild 7" },
+      // { src: MiniHasen8, text: "Mini-Hasen Bild 8" },
+      // { src: MiniHasen9, text: "Mini-Hasen Bild 9" },
+      // { src: MiniHasen10, text: "Mini-Hasen Bild 10" },
+      // { src: MiniHasen11, text: "Mini-Hasen Bild 11" },
+      // { src: MiniHasen12, text: "Mini-Hasen Bild 12" },
+      // { src: MiniHasen13, text: "Mini-Hasen Bild 13" },
     ],
     "Super-Hasen": [
       { src: SuperHasen1, text: "Super-Hasen Bild 1" },
